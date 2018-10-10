cf0ddf8938777f68efaa3faacef73fd409c1206a
hlebbobulka <hlebb@mail.ru>
2018-10-10 10:48:10 +0300

    Updates

---
+++ precache-manifest.8fb3c574a654c5ce178f7cd7591a4961.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1383a3319e701d89a270",
+    "url": "/aviasales-test-task/static/js/runtime~main.1383a331.js"
+  },
+  {
+    "revision": "55178ec4224426213412",
+    "url": "/aviasales-test-task/static/js/main.55178ec4.chunk.js"
+  },
+  {
+    "revision": "09954fe34b45c9125cb3",
+    "url": "/aviasales-test-task/static/js/1.09954fe3.chunk.js"
+  },
+  {
+    "revision": "55178ec4224426213412",
+    "url": "/aviasales-test-task/static/css/main.0e05f79e.chunk.css"
+  },
+  {
+    "revision": "7223b94c9c4e93cb013b9c560b5f5219",
+    "url": "/aviasales-test-task/index.html"
+  }
+];
