940c16f538da4c2abe0ca6e0e871903de59eeb03
hlebbobulka <hlebb@mail.ru>
2018-10-10 10:57:32 +0300

    Fixed url for gh pages

--- src/_constants/endpoints.js
+++ src/_constants/endpoints.js
@@ -1,2 +1,2 @@
-export const tickets = '/data/tickets.json';
+export const tickets = 'http://hlebbobulka.github.io/aviasales-test-task/data/tickets.json';
 export const currency = 'https://www.cbr-xml-daily.ru/daily_json.js';
